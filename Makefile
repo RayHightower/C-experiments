ad7fdfde02916a4e623bf6d7ed4b307de024c07f
RayHightower <rth@wisdomgroup.com>
2011-09-27 08:16:42 -0500

    completed section three

---
+++ Makefile
@@ -0,0 +1,4 @@
+FLAGS=-Wall -g
+
+clean:
+	rm -f ex1
--- ex3.c
+++ ex3.c
@@ -4,7 +4,10 @@ int main()
 {
 
   int age = 12;
+  int height = 80;
+
   printf("I am %d years old \n", age);
+  printf("I am %d inches tall \n", height);
 
   return 0;
 }
